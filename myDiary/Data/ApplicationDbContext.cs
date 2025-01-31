7ae55adaf61c3f667395a6a3d9c24cac16fe94da
Barr Mohammed <barrmohammed.tpa@gmail.com>
2025-01-30 20:53:16 -0500

    added data to database

--- myDiary/Data/ApplicationDbContext.cs
+++ myDiary/Data/ApplicationDbContext.cs
@@ -16,5 +16,30 @@ namespace myDiary.Data
 
         public DbSet<DiaryEntry> DiaryEntries { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            DateTime fixedDate = new DateTime(2024, 1, 30, 12, 0, 0);
+
+            //Creating new diary entry to add to database
+            modelBuilder.Entity<DiaryEntry>().HasData(
+                new DiaryEntry {
+                Id = 1,
+                Title="Went Hiking",
+                Content="Went hiking with Joe!",
+                Created = fixedDate
+
+                },
+
+                new DiaryEntry {
+                    Id = 2,
+                    Title="Went Shopping",
+                    Content="Went shopping with Joe!",
+                    Created = fixedDate
+                }
+            );
+        }
+
     }
 }
---
+++ myDiary/Migrations/20250131015104_FixedSeedingData.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace myDiary.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixedSeedingData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "DiaryEntries",
+                columns: new[] { "Id", "Content", "Created", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Went hiking with Joe!", new DateTime(2024, 1, 30, 12, 0, 0, 0, DateTimeKind.Unspecified), "Went Hiking" },
+                    { 2, "Went shopping with Joe!", new DateTime(2024, 1, 30, 12, 0, 0, 0, DateTimeKind.Unspecified), "Went Shopping" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "DiaryEntries",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "DiaryEntries",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
