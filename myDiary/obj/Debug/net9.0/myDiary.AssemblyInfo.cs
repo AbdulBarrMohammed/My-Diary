760a12ec071c960d4a44723e330093063cdc5c70
Barr Mohammed <barrmohammed.tpa@gmail.com>
2025-01-30 20:10:40 -0500

    added controller

--- myDiary/obj/Debug/net9.0/myDiary.AssemblyInfo.cs
+++ myDiary/obj/Debug/net9.0/myDiary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myDiary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6f1db0e568040c56666ca63b5e2ad2e11aa38fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+405c678cb2ef770ad237bd66880e1cf86124d170")]
 [assembly: System.Reflection.AssemblyProductAttribute("myDiary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myDiary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
