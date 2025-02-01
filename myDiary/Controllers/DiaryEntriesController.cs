f63f8443df54af68a209da4c8fce12d7ccd74cc8
Barr Mohammed <barrmohammed.tpa@gmail.com>
2025-01-31 20:27:20 -0500

    edit diary entry controller

--- myDiary/Controllers/DiaryEntriesController.cs
+++ myDiary/Controllers/DiaryEntriesController.cs
@@ -5,11 +5,18 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using myDiary.Data;
 
 namespace myDiary.Controllers
 {
     public class DiaryEntriesController : Controller
     {
+
+        private readonly ApplicationDbContext _db;
+
+        public DiaryEntriesController(ApplicationDbContext db) {
+            _db = db;
+        }
 
         public IActionResult Index()
         {
