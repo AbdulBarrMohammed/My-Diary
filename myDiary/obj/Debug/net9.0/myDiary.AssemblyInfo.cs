aba2e7f1b587e178df6df4bd6957d7bed8fbed66
Barr Mohammed <barrmohammed.tpa@gmail.com>
2025-01-31 21:03:36 -0500

    styled web app

--- myDiary/obj/Debug/net9.0/myDiary.AssemblyInfo.cs
+++ myDiary/obj/Debug/net9.0/myDiary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myDiary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e9a070cc685d10b7eaab96197e0de2ac1336e8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d212763169492d0c83ded47e3116eaf2378b2bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("myDiary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myDiary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
